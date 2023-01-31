6e07c3d3c861a87fa447ebd419209f8239fb8ce9
kujiu <kujiu@tsign.cn>
2023-01-31 16:02:53 +0800

    ci: fix ci

--- test/main.go
+++ test/main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"log"
 
-	launch_editor "github.com/Binbiubiubiu/launch-editor"
+	. "github.com/Binbiubiubiu/launch-editor"
 )
 
 func main() {
-	err := launch_editor.LaunchEditor("guess.go:10:20")
+	err := LaunchEditor("guess.go:10:20")
 	if err != nil {
 		log.Fatalln(err)
 	}
